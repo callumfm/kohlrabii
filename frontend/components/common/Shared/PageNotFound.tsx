010337ed47d0a2704eeec76509b07fbc5a4fdeb2
Callum <cfmacalister@googlemail.com>
2025-05-27 17:54:13 -0700

    Test change for vercel deployment

--- frontend/components/common/Shared/PageNotFound.tsx
+++ frontend/components/common/Shared/PageNotFound.tsx
@@ -8,7 +8,7 @@ const PageNotFound = () => {
     <div className="flex h-screen w-full flex-col items-center justify-center gap-y-16 px-12">
       <h1 className="text-4xl text-blue-500 dark:text-blue-400">404</h1>
       <h1 className="max-w-xl text-center text-4xl leading-normal">
-        We couldn&apos;t find the page you were looking for
+        We couldn&apos;t find the page you were looking for!
       </h1>
       <Link href={"/"}>
         <Button>Take me home</Button>
